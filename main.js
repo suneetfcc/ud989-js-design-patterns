2f1c8c2c1db99129a6d891b560b67d6c8692de39
Badoni <badsu02@ca.com>
2017-06-12 18:07:56 +0530

    1.11 code improvements

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 (function($window, $document) {
 
-    var model = (function() {
-        var data = [
+    var model = {
+        current: null,
+        cats: [
             {
                 name: 'Pebbles',
                 clicks: 0,
@@ -27,101 +28,69 @@
                 clicks: 0,
                 url: 'img/cat6.jpg'
             }
-        ];
-
-        var addClick = function(id) {
-            data[id].clicks += 1;
-        };
-
-        var clicks = function(id) {
-            return data[id].clicks;
-        };
-
-        var url = function(id) {
-            return data[id].url;
-        };
-
-        var name = function(id) {
-            return data[id].name;
-        };
-
-        var getAll = function() {
-            return data;
-        };
-
-        return {
-            clicks: clicks,
-            url: url,
-            name: name,
-            addClick: addClick,
-            getAll: getAll
-        };
-    })();
+        ]
+    };
 
     var listView = {
         init: function() {
+            this.catListElem = $document.getElementById('cat-list');
             this.render();
         },
         render: function() {
-            var listView = $document.getElementById('cat-list');
-            octopus.getAllNames().forEach(function(name,index) {
+            octopus.getAllCats().forEach(function(cat) {
+                var that = this;
                 var li = $document.createElement('li');
-                var idx = index;
-                li.innerHTML = '<div' + ((index === 0) ? ' class="active">' : '>');
-                li.innerHTML += name + '</div>';
-                li.addEventListener('click', function (event) {
-                    [].forEach.call(listView.children, function(element) {
+                li.textContent = cat.name;
+                that.catListElem.appendChild(li);
+                li.addEventListener('click', function () {
+                    [].forEach.call(that.catListElem.children, function(element) {
                         element.className = '';
                     });
                     li.className = 'active';
-                    detailsView.render(idx);
+                    octopus.setCurrentCat(cat);
                 });
-                listView.appendChild(li);
             }, this);
         }
     };
 
     var detailsView = {
         init: function() {
-            this.render(0);
-        },
-        render: function(id) {
-            var that = this;
-            var detailsView = $document.getElementById('cat-details');
-            detailsView.removeChild(detailsView.firstChild);
-            var html = '<div><h3>' + octopus.name(id) + '</h3>';
-            html    += '<img src="' + octopus.url(id) + '" alt="Meow!!">';
-            html    += '<div class="counter">' + octopus.clicks(id) + ' clicks</div></div>';
-            detailsView.innerHTML = html;
-            var img = detailsView.querySelectorAll('img')[0];
-            img.addEventListener('click', function (e) {
-                octopus.addClick(id);
-                that.render(id);
+            this.catDetailsElem = $document.getElementById('cat-details');
+            this.catNameElem  = $document.getElementById('cat-name');
+            this.catImageElem  = $document.getElementById('cat-image');
+            this.catcounterElem  = $document.getElementById('cat-counter');
+            this.catImageElem.addEventListener('click', function () {
+                octopus.incrementCatCounter();
             });
+            this.render();
+        },
+        render: function() {
+            var cat = octopus.getCurrentCat();
+            this.catNameElem.textContent = cat.name;
+            this.catImageElem.src = cat.url;
+            this.catcounterElem.textContent = cat.clicks;
         }
     };
 
     var octopus = {
         init: function() {
+            model.current = model.cats[0];
             listView.init();
             detailsView.init();
         },
-        name: function(id) {
-            return model.name(id);
+        getCurrentCat: function() {
+            return model.current;
         },
-        url: function(id) {
-            return model.url(id);
+        setCurrentCat: function(cat) {
+            model.current = cat;
+            detailsView.render();
         },
-        clicks: function(id) {
-            return model.clicks(id);
+        incrementCatCounter: function() {
+            model.current.clicks += 1;
+            detailsView.render();
         },
-        getAllNames: function() {
-            return model.getAll().map(function(obj) {
-                return obj.name;
-            });
-        },
-        addClick: function(id) {
-            model.addClick(id);
+        getAllCats: function() {
+            return model.cats;
         }
     };
 
